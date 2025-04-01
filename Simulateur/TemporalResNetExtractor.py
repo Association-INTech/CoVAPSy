c41bc9cb23cd5009aa834bd75ae3a213a32a6406
matthiasbienvenu <matthias.bienvenu@telecom-sudparis.eu>
2025-04-01 14:02:41 +0200

    ???

--- Simulateur/TemporalResNetExtractor.py
+++ Simulateur/TemporalResNetExtractor.py
@@ -109,7 +109,7 @@ class TemporalResNetExtractor(BaseFeaturesExtractor):
         # Compute shape by doing one forward pass
         with torch.no_grad():
             n_flatten = net(
-                torch.zeros([1, 2, lidar_horizontal_resolution], dtype=torch.float32, device=device)
+                torch.zeros([1, 2, 1, lidar_horizontal_resolution], dtype=torch.float32, device=device)
             ).shape[1]
         print("n_flatten: ", n_flatten)
         super().__init__(space, n_flatten)
