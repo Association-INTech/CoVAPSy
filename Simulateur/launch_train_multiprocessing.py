f9034c7229a96735f70dec7200d9a44b3c91196b
matthiasbienvenu <matthias.bienvenu@telecom-sudparis.eu>
2025-04-02 12:33:30 +0200

    ???

--- Simulateur/launch_train_multiprocessing.py
+++ Simulateur/launch_train_multiprocessing.py
@@ -144,7 +144,7 @@ if __name__ == "__main__":
         n_steps=2048,
         n_epochs=20,
         batch_size=256,
-        learning_rate=3e-4,
+        learning_rate=1e-4,
         gamma=0.99,
         verbose=1,
         normalize_advantage=True,
