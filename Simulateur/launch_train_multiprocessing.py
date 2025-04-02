9d90e3cfa8bc7e5ba3c770fa5d4cdeb80f6d81a6
matthiasbienvenu <matthias.bienvenu@telecom-sudparis.eu>
2025-04-02 11:26:50 +0200

    ???

--- Simulateur/launch_train_multiprocessing.py
+++ Simulateur/launch_train_multiprocessing.py
@@ -142,7 +142,7 @@ if __name__ == "__main__":
 
     ppo_args = dict(
         n_steps=2048,
-        n_epochs=20,
+        n_epochs=10,
         batch_size=512,
         learning_rate=3e-4,
         gamma=0.99,
