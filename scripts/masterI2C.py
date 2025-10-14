0250a1afcede1067a82e31c1de8fad59ad63d519
Danura30082 <danura30082@gmail.com>
2025-10-14 23:24:27 +0200

    Revert "Merge pull request #4 from Cbampeta/main"
    This reverts commit 2307b8293cb06ef9ce514f71705fe4334ff0df83, reversing
    changes made to 5a3dff5ffe60656b9484e6786a0b398140882f1e.

--- scripts/masterI2C.py
+++ scripts/masterI2C.py
@@ -6,13 +6,13 @@ bus = smbus.SMBus(1)  # 1 indicates /dev/i2c-1
 
 # I2C address of the slave
 SLAVE_ADDRESS = 0x08
-"""
+
 def write_data(data):
     # Convert string to list of ASCII values
     data_list = [ord(char) for char in data]
-    bus.write_i2c_block_data(SLAVE_ADDRESS, 48, data_list)
-"""
-"""
+    bus.write_i2c_block_data(SLAVE_ADDRESS, 0, data_list)
+
+<<<<<<< HEAD:scripts/masterI2C.py
 import struct
 
 def read_data(length):
@@ -24,33 +24,22 @@ def read_data(length):
         return float_value
     else:
         raise ValueError("Not enough data received from I2C bus")
-"""
-
-
-def write_data(data):
-    try:
-        bus.write_byte(I2C_SLAVE_ADDRESS, value_to_send)
-        print("sending value:", value_to_send)
-    except Exception as e:
-        print("Erreur écriture:", e)
-
-def read_data(addr):
-    try:
-        n = bus.read_byte(I2C_SLAVE_ADDRESS)
-        print("new received value:", n)
-    except Exception as e:
-        print("Erreur lecture:", e)
-
+=======
+def read_data(length):
+    # Read a block of data from the slave
+    data = bus.read_i2c_block_data(SLAVE_ADDRESS, 0, length)
+    # Convert list of ASCII values to string
+    return ''.join(chr(byte) for byte in data)
+>>>>>>> origin/oled_annimation:HL/masterI2C.py
 
 if __name__ == "__main__":
     try:
         # Send data to the slave
-        write_data(23)
-        print("sending value: "+str(23))
+        write_data("Hello Slave!")
         time.sleep(1)  # Wait for the slave to process the data
 
         # Request data from the slave
-        received = read_data(SLAVE_ADDRESS)  # Adjust length as needed
+        received = read_data(13)  # Adjust length as needed
         print("Received from slave:", received)
 
     except Exception as e:
