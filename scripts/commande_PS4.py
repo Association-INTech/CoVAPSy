511d2396dbce2ed1f8b7233f75a0cf87bcce7edc
cbampeta <cyril.bampeta@telecom-sudparis.eu>
2025-10-30 19:14:00 +0100

    feat: suppression de masterI2C dans script (copie présente dans HL) et remise en forme commande_PS4

--- scripts/commande_PS4.py
+++ scripts/commande_PS4.py
@@ -1,61 +1,63 @@
 from pyPS4Controller.controller import Controller
 import time
-from masterI2C import write_vitesse_direction
 from threading import Thread
+
+#Pour le protocole I2C de communication entre la rasberie Pi et l'arduino
+import smbus #type: ignore #ignore the module could not be resolved error because it is a linux only module
+import numpy as np
+import struct
+
+###################################################
+#Intialisation du protocole I2C
+##################################################
+
+# Create an SMBus instance
+bus = smbus.SMBus(1)  # 1 indicates /dev/i2c-1
+
+# I2C address of the slave
+SLAVE_ADDRESS = 0x08
+
+def write_vitesse_direction(vitesse,direction):
+    # Convert string to list of ASCII values
+    data = struct.pack('<ff', float(vitesse), float(direction))
+    bus.write_i2c_block_data(SLAVE_ADDRESS, 0, list(data))
 
 ###################################################
 #Intialisation des moteurs
 ##################################################
 
-direction_d = 90
-vitesse_m = 0
+direction_d = 90 # angle initiale des roues en degrés
+vitesse_m = 0   # vitesse initiale en métre par milliseconde
+
 #paramètres de la fonction vitesse_m_s, à étalonner
-direction_prop = 1# -1 pour les variateurs inversés ou un petit rapport correspond à une marche avant
-pwm_stop_prop = 7.37
-point_mort_prop = 0.5
-delta_pwm_max_prop = 1.1 #pwm à laquelle on atteint la vitesse maximale
+vitesse_max_m_s_hard = 8 #vitesse que peut atteindre la voiture en métre
+vitesse_max_m_s_soft = 2 #vitesse maximale que l'on souhaite atteindre en métre par seconde
+vitesse_min_m_s_soft = -2 #vitesse arriere que l'on souhaite atteindre en métre
 
-vitesse_max_m_s_hard = 8 #vitesse que peut atteindre la voiture
-vitesse_max_m_s_soft = 2 #vitesse maximale que l'on souhaite atteindre
-vitesse_min_m_s_soft = -2 #vitesse arriere que l'on souhaite atteindre
-
-direction = -1 #1 pour angle_pwm_min a gauche, -1 pour angle_pwm_min à droite
-angle_pwm_min = 6.91  #min
-angle_pwm_max = 10.7  #max
-angle_pwm_centre= 8.805
 angle_degre_max = +18 #vers la gauche
-angle_degre=0
 
-def envoie_direction_degre():
-    while True :
-        write_vitesse_direction(int(vitesse_m), int(direction_d))
-        time.sleep(0.001)
-#on recode map de arduino pour des soucis 
 
+
+# fonction naturel map de ardiono pour plus de lisibilité
 def map(x, in_min,in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
 
 
 def set_direction_degre(angle_degre) :
     global direction_d
     direction_d = angle_degre
     print("angle_degré: ",direction_d,"vitesse: ",vitesse_m)
-    #write_vitesse_direction(int(vitesse_m), int(direction_d)) #take the angle in degrees
     
 def set_vitesse_m_ms(vitesse_m_ms):
     global vitesse_m
     vitesse_m = vitesse_m_ms
     print("angle_degré: ",direction_d,"vitesse: ",vitesse_m)
-    #write_vitesse_direction(int(vitesse_m), int(direction_d)) # Convert to millimeters per second
         
-def recule():
+def recule(): #actuellement ne sert a rien car on peux juste envoyer une vitesse négative 
     global vitesse_m
     vitesse_m = -2000
-    #write_vitesse_direction(int(vitesse_m), int(direction_d)) # Convert to millimeters per second
 
-
-a_prop = vitesse_max_m_s_soft/(65198)
-a_dir=(angle_degre_max)/(-32767) 
 
 class MyController(Controller):
 
@@ -73,7 +75,7 @@ class MyController(Controller):
     def on_L3_x_at_rest(self):
         set_direction_degre(90)
         
-    def on_R1_press(self):
+    def on_R1_press(self): #arret d'urgence
         set_vitesse_m_ms(0)
         
     def on_R1_release(self):
@@ -95,16 +97,15 @@ class MyController(Controller):
             set_vitesse_m_ms(0)
         else:
             set_vitesse_m_ms(vit)
-        
-    # def on_R3_right(self, value):
-    #     set_vitesse_m_s(value*a_prop)
-        
-    # def on_R3_left(self, value):
-    #     set_vitesse_m_s(map(-32252,32767,0,vitesse_max_m_s_soft))
+
+#envoie de la direction et de l'angle toute les millisecondes
+def envoie_direction_degre():
+    while True :
+        write_vitesse_direction(int(vitesse_m), int(direction_d))
+        time.sleep(0.001)
 
 
-
-
+# boucle principal
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 try:
     Thread(target = envoie_direction_degre, daemon=True).start()
@@ -112,8 +113,6 @@ try:
 
 except KeyboardInterrupt:
     print("Arrêt du programme")
-    pwm_prop.stop()
-    pwm_dir.stop()
     controller.stop()
     exit(0)
 
