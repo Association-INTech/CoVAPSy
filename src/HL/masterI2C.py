afde8be6fe7289ed597df85125a5ceb55937fa5f
cbampeta <cyril.bampeta@telecom-sudparis.eu>
2025-11-05 23:29:37 +0100

    test dansd mzster I2C

--- src/HL/masterI2C.py
+++ src/HL/masterI2C.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
         write_vitesse_direction(vitesse,rotation)
         time.sleep(0.1)  # Wait for the slave to process the data
         received = read_data(3)  # Adjust length as needed
-        print("Received from slave:", received)
+        print("Received from slave:", received[0],received[1], received[2])
 
         # Request data from the slave
         
