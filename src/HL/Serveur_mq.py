7c8225f9056807913c2a9163bf6282278f615338
cbampeta <cyril.bampeta@telecom-sudparis.eu>
2025-11-15 05:40:06 +0100

    test de rajoue d'information dans Serveur_mq

--- src/HL/Serveur_mq.py
+++ src/HL/Serveur_mq.py
@@ -6,6 +6,18 @@ import threading
 import smbus
 import logging as log
 import struct
+
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import ssd1306
+from PIL import Image, ImageDraw, ImageFont
+from gpiozero import LED, Button, Buzzer
+import textwrap
+
+from get_ip import get_ip, check_ssh_connections
+import subprocess
+serial = i2c(port=1, address=0x3C)
+device = ssd1306(serial)
 #on démarre les log
 log.basicConfig(level=log.INFO)
 
@@ -15,6 +27,14 @@ bus = smbus.SMBus(1)  # 1 indicates /dev/i2c-1
 SLAVE_ADDRESS = 0x08
 
 length_i2c_received = 3 #le nombre de donnée récupéré par l'i2c
+
+bp_next = Button("GPIO5", bounce_time=0.1)
+bp_entre = Button("GPIO6", bounce_time=0.1)
+led1 = LED("GPIO17")
+led2 = LED("GPIO27")
+buzzer = Buzzer("GPIO26")
+TEXT_HEIGHT = 11
+TEXT_LEFT_OFFSET = 3 # Offset from the left of the screen to ensure no cuttoff
 
 # on recoit les inoformations
 received = context.socket(zmq.REP)
@@ -30,7 +50,112 @@ voltage_nimh = 0
 initial_time = time.time()
 last_cmd_time = 0
 
+ip = get_ip
 
+programme = {
+    1: {
+        "name" : "Auto Driving",
+        "type" : "python",
+        "path" : "",
+        "info" : "Ia for CoVaPSY"
+    },
+    2: {
+        "name" : "Manual Driving With PS4 Controller",
+        "path" : "./scripts/commande_PS4.py",
+        "info" : "Commande_PS4 from bluetooth"
+    },
+    3: {
+        "name" : "Connect PS4 Controller",
+        "type" : "bash",
+        "path" : "./scripts/bluetooth_auto/bluethootconnect.sh",
+        "info" : "Autoconnect Controller"
+    }
+}
+
+Screen = 0
+State = 0
+#-----------------------------------------------------------------------------------------------------
+# fonction utile
+#-----------------------------------------------------------------------------------------------------
+def make_voltage_im():
+    try: #Will fail if arduino is rest ex: temporary power loss when plugu=ing in usb
+        received = [voltage_lipo , voltage_nimh]  # Adjust length as needed
+    except OSError:
+        received = [0.0, 0.0]
+        print("I2C bus error")
+    
+    # filter out values below 6V and round to 2 decimal places
+    received = [round(elem, 2) if elem > 6 else 0.0 for elem in received]
+    text = f"LiP:{received[0]:.2f}V|NiH:{received[1]:.2f}V"
+    im = Image.new("1", (128, TEXT_HEIGHT), "black")
+    draw = ImageDraw.Draw(im)
+    font = ImageFont.load_default()
+    draw.text((3, 0), text, fill="white", font=font)
+    return im
+
+def display_combined_im(text):
+    im = Image.new("1", (128, 64), "black")
+    draw = ImageDraw.Draw(im)
+    font = ImageFont.load_default()
+    
+    # Wrap the text to fit within the width of the display
+    wrapped_text = textwrap.fill(text, width=20)  # Adjust width as needed
+    draw.text((3, 0), wrapped_text, fill="white", font=font)
+    
+    voltage_im = make_voltage_im()
+    im.paste(voltage_im, (0, 64 - TEXT_HEIGHT))
+    
+    with canvas(device) as draw:
+        draw.bitmap((0, 0), im, fill="white")
+
+
+def Idle(): #Enable chossing between states
+    global Screen
+    global State
+    if Screen==0 and check_ssh_connections():
+        led1.on()
+        Screen=1
+    if not check_ssh_connections():
+        led1.off()
+    match Screen: #Display on OLED
+        case 0: #IP and ssh status
+            ip=get_ip()
+            text = "Ready to SSH\nIP:"+ip
+        case 1: #AutoDriving mode
+            text = "Auto Driving"
+        case 2: #Manual Driving mode
+            text = "Manual Driving With PS4 Controller"
+            #PS4 controller status
+        case 3: #Kill all processus
+            text = "Kill all the process"
+    
+    if (Screen == 0):
+        ip=get_ip()
+            text = "Ready to SSH\nIP:"+ip
+    elif (Screen <= len(programme)):
+        text = programme[Screen]
+
+    if (State==Screen):
+        text+=": (en cours)"
+
+    display_combined_im(text)
+
+
+    if bp_next.is_pressed:
+        bp_next.wait_for_release()
+        Screen+=1
+        if Screen>len(programme):
+            Screen=0
+    if bp_entre.is_pressed:
+        bp_entre.wait_for_release() 
+        State=Screen
+        if (Screen == 3): #pour teste
+            subprocess.run("./bluethootconnect.sh", shell=True, check=True)
+
+
+#---------------------------------------------------------------------------------------------------
+# fonction pour la communication
+#---------------------------------------------------------------------------------------------------
 def i2c_loop():
     """Envoie vitesse/direction régulièrement au microcontroleur."""
     global vitesse_d, direction, last_cmd_time
