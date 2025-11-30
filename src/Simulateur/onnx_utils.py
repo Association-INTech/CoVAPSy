e5e6fdb6f7694fa566be93e70b663b63c9ec3a0b
ExoRoy <nayachid@hotmail.fr>
2025-11-30 21:16:31 +0100

    Mathias's issues give to Nachid test 1

--- src/Simulateur/onnx_utils.py
+++ src/Simulateur/onnx_utils.py
@@ -3,7 +3,7 @@ from stable_baselines3 import PPO
 import torch.nn as nn
 import torch
 from config import *
-
+import numpy as np
 from CNN1DExtractor import CNN1DExtractor
 from TemporalResNetExtractor import TemporalResNetExtractor
 
@@ -38,23 +38,23 @@ def export_onnx(model):
     model.policy.to(device)
     model.policy.train()
 
+def run_onnx_model(session : ort.InferenceSession,x : np.ndarray):
+
+    return session.run(None, {"input": x})[0]
 
 def test_onnx(model):
     device = model.policy.device
     model.policy.eval()
     true_model = get_true_model(model)
 
+    loss_fn = nn.MSELoss()
+    x = torch.randn(1000, 2, context_size, lidar_horizontal_resolution)
+
     try:
         session = ort.InferenceSession("model.onnx")
     except Exception as e:
         print(f"Error loading ONNX model: {e}")
         return
-
-    def model_onnx(x):
-        return session.run(None, {"input": x.cpu().numpy()})[0]
-
-    loss_fn = nn.MSELoss()
-    x = torch.randn(1000, 2, context_size, lidar_horizontal_resolution)
 
     with torch.no_grad():
         y_true_test = true_model(x)
@@ -63,7 +63,7 @@ def test_onnx(model):
         y_true_train = true_model(x)
         true_model.eval()
 
-        y_onnx = model_onnx(x)
+        y_onnx = run_onnx_model(session,x.cpu().numpy())
 
         loss_test = loss_fn(y_true_test, torch.tensor(y_onnx))
         loss_train = loss_fn(y_true_train, torch.tensor(y_onnx))
