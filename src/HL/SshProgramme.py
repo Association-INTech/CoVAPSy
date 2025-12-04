f3358430a2ef7c872bf9b0a9db6f05e2882cf51f
cbampeta <cyril.bampeta@telecom-sudparis.eu>
2025-12-04 21:30:12 +0100

    fix : rajout de la possibilité de mettre la voiture en stand by.

--- src/HL/SshProgramme.py
+++ src/HL/SshProgramme.py
@@ -4,12 +4,19 @@ from get_ip import get_ip, check_ssh_connections
 class SshProgramme(Program):
     def __init__(self):
         self.name = "Ssh to:" + get_ip()
-        self.running = False
+        self.running = True
         self.controls_car = True
 
         self.vitesse_d = 0
         self.direction = 0
     
+
+    def start(self):
+        self.running = True
+    
+    def stop(self):
+        self.running = False
+        
     def display(self):
         text = self.name
         if check_ssh_connections():
