a494307a0d8ef2ff388b9d0f8aa4b493ba6a3ce0
ExoRoy <nachid.raslane@telecom-sudparis.eu>
2026-01-10 00:14:55 +0100

    tentative d'inversion des maps

--- src/Simulateur/config.py
+++ src/Simulateur/config.py
@@ -1,7 +1,7 @@
 # just a file that lets us define some constants that are used in multiple files the simulation
 from torch.cuda import is_available
 
-n_map = 2
+n_map = 4
 n_simulations = 8
 n_vehicles = 1
 n_stupid_vehicles = 0
